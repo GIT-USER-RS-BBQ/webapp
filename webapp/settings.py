195d26a0908a62e1f0c48d5a389d4c49ec93b7cf
GIT-USER-RS-BBQ <git@sarikaya-software-and-education.com>
2024-06-14 12:07:35 +0200

    Allowed_HOST set to *

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*stxoypk68om(gqbu3=@%x7v#avmt8w56ls%k%za=e(hff8tts
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
